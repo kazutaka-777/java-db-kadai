b2586a82425b98e85c232dc86fed1b0579a45c5b
kazutaka-777 <cappuccino.kurage@gmail.com>
2025-03-05 12:36:24 +0900

    Update Posts_Chapter07.java

--- kadai_007/Posts_Chapter07.java
+++ kadai_007/Posts_Chapter07.java
@@ -26,13 +26,18 @@ public class Posts_Chapter07 {
             statement = con.createStatement();
             System.out.println("データベース接続成功:"+con);
             String sql = "INSERT INTO posts (user_id, posted_at,post_content,likes) VALUES "
-            		+ " ('1003','2023-02-08','昨日の夜は徹夜でした・・','13'),"
-            		+ " ('1002','2023-02-08','お疲れ様です！','12'),"
-            		+ " ('1003','2023-02-09','今日も頑張ります！','18'),"
-            		+ " ('1001','2023-02-09','無理は禁物ですよ！','17'),"
-            		+ " ('1002','2023-02-10','明日から連休ですね！','20')"
+            		+ " (1003,'2023-02-08','昨日の夜は徹夜でした・・',13),"
+            		+ " (1002,'2023-02-08','お疲れ様です！',12),"
+            		+ " (1003,'2023-02-09','今日も頑張ります！',18),"
+            		+ " (1001,'2023-02-09','無理は禁物ですよ！',17),"
+            		+ " (1002,'2023-02-10','明日から連休ですね！',20)"
             		+ ";";
             System.out.println("レコード追加を実行します");
+            
+         // INSERTクエリの前にこの行を追加
+            String truncatesql = "TRUNCATE TABLE posts;";
+            statement.executeUpdate(truncatesql);
+            
            int rowCnt = statement.executeUpdate(sql);
            System.out.println(rowCnt + "件のレコードが追加されました");
            String sqlSelect = "SELECT posted_at,post_content,likes FROM posts WHERE user_id = 1002;";
@@ -45,12 +50,10 @@ public class Posts_Chapter07 {
            
         // SQLクエリの実行結果を抽出
            while(result.next()) {
-        	   int row = 0;
         	   Date postedAt = result.getDate("posted_at");
         	   String postContent = result.getString("post_content");
         	   int likes = result.getInt("likes");
-        	   row++;
-               System.out.println(row + "件目: 投稿日時=" + postedAt + ", 投稿内容=" + postContent + ", いいね数=" + likes);
+               System.out.println(result.getRow() + "件目: 投稿日時=" + postedAt + ", 投稿内容=" + postContent + ", いいね数=" + likes);
            }
 
         } catch(SQLException e) {
